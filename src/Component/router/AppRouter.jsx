88e0e8644c0ed216060e2fcf6029fb3e74e202cd
Cosmo <121879395+Cosmosynth@users.noreply.github.com>
2026-08-15 15:01:12 +0530

    Added Dashboard

--- src/Component/router/AppRouter.jsx
+++ src/Component/router/AppRouter.jsx
@@ -2,6 +2,9 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Login } from "../common/Login"
 import { Signup } from "../common/Singup"
 import { HeroPage } from "../pages/HeroPage"
+import { Dashboard } from "../pages/Dasboard"
+import { LiveIncident } from "../pages/LiveIncident"
+import { AnalyticsReports } from "../pages/AnalyticsReports"
 
 const router = createBrowserRouter([
     {
@@ -16,6 +19,18 @@ const router = createBrowserRouter([
         path:"/signup",
         element:<Signup/>
     },
+    {
+        path:"/admin",
+        element:<Dashboard/>
+    },
+    {
+        path:"/admin/incident",
+        element:<LiveIncident/>
+    },
+    {
+        path:"/admin/analytics",
+        element:<AnalyticsReports/>
+    },
 
 ])
 const AppRouter = ()=>{
@@ -23,3 +38,4 @@ const AppRouter = ()=>{
     return <RouterProvider router={router}></RouterProvider>
 }
 export default AppRouter
+
